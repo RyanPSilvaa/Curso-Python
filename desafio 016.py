dae81a1e445201b612ba0c454d6c50b473cc76ab
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2024-10-15 22:06:19 -0300

    Adicionai mais arquivos

---
+++ desafio 016.py
@@ -0,0 +1,9 @@
+#modo 1
+import math
+num = float(input('Digite um Valor:'))
+print('O valor digitado foi {} e a porção inteira dele é {}'.format(num, math.trunc(num)))
+
+#modo 2
+num = float(input('Digite um Valor:'))
+print('O valor digitado foi {} e a porção inteira dele é {}'.format(num, int(num)))
+
---
+++ desafio 017.py
@@ -0,0 +1,5 @@
+import math
+co = float(input('Comptimento do cateto oposto:'))
+ca = float(input('Comp´rimento do cateto adjacente:'))
+hi = math.hypot(co, ca)
+print(hi)
